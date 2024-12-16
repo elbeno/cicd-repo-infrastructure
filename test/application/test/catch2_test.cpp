d5a4811b6578eedb91327c863a44a4b012f3426d
Ben Deane <ben@elbeno.com>
2024-12-16 09:01:30 -0700

    :arrow_up: Update various dependencies
    - Catch2: 3.6.0 -> 3.8.0
    - CPM: 0.38.2 -> 0.40.2
    - GoogleTest: 1.14.0 -> 1.15.2
    - rapidcheck: 1c91f40 -> ff6af6f
    - Snitch: 1.2.5 -> 1.3.1
    - Mull: 0.23.0 -> 0.24.0
    - Switch to clang-19 as the primary compiler for tests in this repo
    
    Note:
    - A recent change to puppeteer caused mermaid doc image generation to fail. Fixed.
    - Catch2 has a bug with C++17 and clang-19 detailed at https://github.com/catchorg/Catch2/issues/2910

--- test/application/test/catch2_test.cpp
+++ test/application/test/catch2_test.cpp
@@ -1,7 +1,10 @@
+#include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <rapidcheck/catch.h>
 
 TEST_CASE("catch2 test", "[test]") {}
+
+TEMPLATE_TEST_CASE("catch2 template test", "[test]", int) {}
 
 TEST_CASE("catch2 with rapidcheck", "[test]") {
     rc::prop("test property", [](unsigned int a) { return a * 2u == a + a; });
