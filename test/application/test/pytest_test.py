4e965ada990dca3964b98ef9664b51c3342f8b25
Ben Deane <ben@elbeno.com>
2026-01-26 11:09:43 -0700

    wip

--- test/application/test/pytest_test.py
+++ test/application/test/pytest_test.py
@@ -2,7 +2,11 @@ from hypothesis import given, strategies as st
 
 small_ints = st.integers(min_value=0, max_value=1000)
 
+import sys
+
+
 
 @given(small_ints, small_ints)
 def test_addition_is_commutative(a: int, b: int):
+    assert False, f"python is {sys.executable}"
     assert a + b == b + a
